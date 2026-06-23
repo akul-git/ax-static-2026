19f6369c76568de557ce44416959fc55ff0fef76
Arun Kulkarni <akul2510@gmail.com>
2026-06-23 13:27:34 +0530

    ax logo added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,ts,jsx,tsx}", // include Astro files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
